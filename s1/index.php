92bb7f0d84eea649e14573bef0e53de37e203d91
unknown <med.harik@gmail.com>
2020-02-02 16:03:50 +0000

    php , les variables , shortcut, les conditions

--- s1/index.php
+++ s1/index.php
@@ -1,19 +1,20 @@
 <?php 
-$prenom="Mohamed";
+$t=[12,2,20,9,8,10];
+$maj=0;
+for ($i=0; $i < count($t); $i++) ( $t[$i]>18) &&  $maj++;
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>test</title>
-  </head>
-  <body>
-   <h2>
-     Bienvenue <?php echo $prenom;?>
-
-</ul>
-   </h2>
-  </body>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+ <ul>
+   <li><?=$maj?> majeur<?=(($maj>1)? "s":"")?></li>
+   <li><?=(count($t)-$maj) ?> mineur<?=(((count($t)-$maj)>1)? "s":"")?></li>
+ </ul>
+</body>
 </html>
