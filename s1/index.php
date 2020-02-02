9b9a1fa6f1934e8db3b8e973734ac1c08e9cd41e
unknown <med.harik@gmail.com>
2020-02-02 11:51:46 +0000

    premier cours php

---
+++ s1/index.php
@@ -0,0 +1,19 @@
+<?php 
+$prenom="Mohamed";
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>test</title>
+  </head>
+  <body>
+   <h2>
+     Bienvenue <?php echo $prenom;?>
+
+</ul>
+   </h2>
+  </body>
+</html>
